061b71a87aaa363a2f5a21cb4fef944e2ef5fb6d
Alexandre Borges <alexandre.bsilva55@gmail.com>
2020-06-19 01:49:45 -0300

    final adjusts

--- src/pages/ImagesList.js
+++ src/pages/ImagesList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Gallery from 'react-photo-gallery'
 import { Card, Alert, } from 'react-bootstrap';
 import socketIOClient from 'socket.io-client'
 import { Link } from 'react-router-dom'
@@ -13,22 +12,48 @@ class ImageList extends Component {
         this.socket = socketIOClient(process.env.REACT_APP_SERVER_URL)
     }
 
+
     render() {
         let collectionsComponents = []
+
         this.socket.on('listImages', (receivedImages) => {
             this.setState({ loading: receivedImages.loading })
             this.setState({ images: receivedImages.images })
 
-            receivedImages.images.map((image, idxDomain) => {
+            receivedImages.images.map((image, idxCol) => {
                 let photos = []
-                image.images.map((i) => {
-                    photos.push({ src: i.url, width: 4, height: 3 })
+                image.images.map((i, idxPhoto) => {
+                    photos.push(
+                        <tr key={idxPhoto}>
+                            <td>
+                                {i.url}
+                            </td>
+                            <td>
+                                {i.contentType}
+                            </td>
+                            <td>
+                                <img src={i.url} key={i.url} alt='imgSeeker' height='70px' width='70px'></img>
+                            </td>
+                        </tr>
+                    )
                 })
                 collectionsComponents.push((
-                    <Card key={Math.random()} className="mt-3">
-                        <Card.Title>{image._id} - {photos.length} imagens encontradas</Card.Title>
-                        <Gallery photos={photos} />
-                    </Card>)
+                    < Card key={Math.random()} className="mt-3" >
+                        <Card.Title>{image._id} - {photos.length} found images</Card.Title>
+                        <table className='table table-striped'>
+                            <thead>
+                                <tr>
+                                    <th>url</th>
+                                    <th>contentType</th>
+                                    <th>Imagem</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {photos.map((photo) => (photo))}
+                            </tbody>
+                        </table>
+
+                    </Card >)
                 )
             })
             this.setState({ collections: collectionsComponents })
@@ -36,12 +61,14 @@ class ImageList extends Component {
 
         return (
             <>
-                {this.state.collections.length === 0 ?
-                    <>
-                        <Alert variant='warning'>There are no saved images</Alert>
-                        <br />
-                        <Link to='/images-list'>Search images</Link></> :
-                    null}
+                {
+                    this.state.collections.length === 0 ?
+                        <>
+                            <Alert variant='warning'>There are no saved images</Alert>
+                            <br />
+                            <Link to='/images-list'>Search images</Link></> :
+                        null
+                }
                 {this.state.collections.map((domain) => (domain))}
             </>
 
